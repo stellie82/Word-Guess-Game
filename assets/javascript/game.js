a8a46d29f0aaece1369c5e575c85c982beb02dd3
Stella Kim <stella@Stellas-MacBook-Pro.local>
2019-09-10 16:20:44 -0700

    add keyup, random word guesser and letter array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,40 +20,54 @@ var movies = [
 ]
 
 var chosenMovie;
-var lettersMovie = [];
+var letterArray
+var correctGuesses = [];
+var wrongGuesses = [];
+var remainingGuesses;
 
 // Create variables to hold the number of wins, guesses, and losses.  Guesses to start at 9 and count down.
 var wins = 0;
 var losses = 0;
-var guesses;
+
+// Create variables to hold references in the HTML to display information.
+var directionsText = document.getElementById("directions-text");
+var userGuessText = document.getElementById("userguess-text");
+var guessesText = document.getElementById("guesses-text");
+var winsText = document.getElementById("wins-text");
 
 // Choose a random word from the movies list.
 var chosenMovie = movies[Math.floor(Math.random() * movies.length)];
 
-for (var i = 0; i < chosenMovie.length; i++) {
-    lettersMovie[i] = "_";
-}
-
-var remainingGuesses = randomWord.length;
-
-// Create variables to hold references in the HTML to display information.
-var userGuessText = document.getElementById("userguess-text");
-var directionsText = document.getElementById("directions-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesText = document.getElementById("guesses-text");
-
-
+console.log(chosenMovie);
 
 document.onkeyup = function (event) {
 
-    var userGuess = event.key;
+    var userGuess = event.key.toLowerCase();
+
+    letterArray = chosenMovie.split("");
+    console.log(letterArray);
+
+    for (var i = 0; i < letterArray.length; i++) {
+        if (userGuess == letterArray[i]) {
+            correctGuesses.push(userGuess);
+            userGuessText.innerHTML = correctGuesses;
+        }
+        
+        else {
+            wrongGuesses.push(userGuess);
+            guessesText.innerHTML = wrongGuesses;
+        }
+    }
+
+    
 
 }
 
+var remainingGuesses = chosenMovie.length;
 
 
 
 
-   
-};
+
+
+
