15b66c0165520553ba0599fad3a09955be177400
Stella Kim <stella@Stellas-MacBook-Pro.local>
2019-09-11 13:05:38 -0700

    take out movies, update keyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,51 +13,47 @@ var movies = [
     "Star Wars",
     "Star Trek",
     "Wall-E",
-    "Apollo 13",
-    "Gravity",
-    "2001: A Space Odyssey",
-    "The Martian"
 ]
 
 var chosenMovie;
 var letterArray = [];
 var correctGuesses = [];
 var wrongGuesses = [];
-var remainingGuesses;
 
 // Create variables to hold the number of wins, guesses, and losses.  Guesses to start at 9 and count down.
 var wins = 0;
 var losses = 0;
-
-
+var remainingGuesses;
 
 // Choose a random word from the movies list.
 var chosenMovie = movies[Math.floor(Math.random() * movies.length)];
 
 console.log(chosenMovie);
+letterArray = chosenMovie.split("");
+console.log(letterArray);
 
 document.onkeyup = function (event) {
 
     // Create variables to hold references in the HTML to display information.
     var directionsText = document.getElementById("directions-text");
     var userGuessText = document.getElementById("userguess-text");
+    var wrongGuessText = document.getElementById("wrong-text");
     var guessesText = document.getElementById("guesses-text");
     var winsText = document.getElementById("wins-text");
 
     var userGuess = event.key.toLowerCase();
 
-    letterArray = chosenMovie.split("");
-    console.log(letterArray);
-
+    var letterPosition = -1;
     for (var i = 0; i < letterArray.length; i++) {
         if (userGuess == letterArray[i]) {
-            correctGuesses.push(userGuess);
-            userGuessText.innerHTML = correctGuesses;
+            letterPosition = i;
+            correctGuesses.push(letterArray[i]);
+            userGuessText.innerHTML = correctGuesses.join(" ");
         }
 
         else {
-            wrongGuesses.push(userGuess);
-            guessesText.innerHTML = wrongGuesses;
+            wrongGuesses.push(letterArray[i]);
+            wrongGuessText.innerHTML = wrongGuesses.join(" ");
         }
     }
 
@@ -66,7 +62,6 @@ document.onkeyup = function (event) {
 }
 
 // var remainingGuesses = chosenMovie.length;
-
 
 
 
